02b5fb30a0758753d66d1cd7ebda36a476e30cc4
KimJiWhan <jiwhan1015@yonsei.ac.kr>
2022-01-05 20:55:55 +0900

    220105 Third Commit - Making Functions

--- src/components/views/LoginView/index.jsx
+++ src/components/views/LoginView/index.jsx
@@ -1,28 +1,64 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import { Link } from "react-router-dom";
+import Moment from "react-moment";
+import moment from "moment";
+import "moment/locale/ko";
 
 function LoginView() {
+  const currentMonth = moment().format("mm");
+  const currentDay = moment().format("dd");
+  const currentHour = moment().format("hh");
+  const currentMin = moment().format("mm");
+  const [hour, setHour] = useState();
+  const [min, setMin] = useState();
+  let aimTime = 0;
+  const onHourChange = (e) => {
+    setHour(e.target.value);
+  };
+  const onMinChange = (e) => {
+    setMin(e.target.value);
+  };
+  const onClickButton = (e) => {
+    aimTime = hour * 60 + min;
+    if (aimTime > currentHour * 60 + currentMin) {
+    } else {
+    }
+  };
   return (
     <OutLineBox>
       <LoginBox>
-        <ExplainBox>연세대학교 새내기 모의수강신청</ExplainBox>
-        <table>
-          <tr>
-            <td>시</td>
-            <InputNum placeholder="'시'를 입력하시오." />
-            <td rowSpan="2">
-              <Link to="/main">
-                <ProceedBtn />
-              </Link>
-            </td>
-          </tr>
-          <tr>
-            <td>분</td>
-            <InputNum placeholder="'분'을 입력하시오." />
-          </tr>
-        </table>
+        <ExplainStr>연세대학교 새내기 모의수강신청</ExplainStr>
+        <TimeBox>
+          <table>
+            <tr>
+              <td style={{ paddingRight: "1rem", fontSize: "1.4rem" }}>시</td>
+              <td>
+                <InputNum
+                  placeholder="'시'를 입력하시오."
+                  value={hour}
+                  onChange={onHourChange}
+                />
+              </td>
+            </tr>
+            <tr>
+              <td style={{ paddingRight: "1rem", fontSize: "1.4rem" }}>분</td>
+              <td>
+                <InputNum
+                  placeholder="'분'을 입력하시오."
+                  value={min}
+                  onChange={onMinChange}
+                />
+              </td>
+            </tr>
+          </table>
+          <ProceedBtn onClick={onClickButton}>시작</ProceedBtn>
+        </TimeBox>
       </LoginBox>
+      <p>
+        {hour} {min}
+      </p>
+      <Moment format="HH"></Moment>
     </OutLineBox>
   );
 }
@@ -38,9 +74,17 @@ const LoginBox = styled.div`
   width: 47.6rem;
   height: 29rem;
   background: url("/images/login.png");
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 `;
-const ExplainBox = styled.div`
-  margin: 10rem 0rem 0rem 0rem;
+const TimeBox = styled.div`
+  display: flex;
+  flex-direction: row;
+`;
+const ExplainStr = styled.p`
+  margin: 10rem 0rem 4rem 0rem;
   font-size: 2.5rem;
   font-weight: 1000;
   color: #1a365e;
@@ -49,9 +93,24 @@ const InputNum = styled.input`
   width: 15.1rem;
   height: 2.7rem;
   border-radius: 0rem;
-  border: 0.1rem solid #aaaaaa;
-  font-size: 1rem;
+  border: 0.1rem solid #dddddd;
+  font-size: 1.4rem;
+  margin-bottom: 0.5rem;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
 `;
-const ProceedBtn = styled.button`
-  height: 5rem;
+const ProceedBtn = styled.div`
+  height: 5.9rem;
+  width: 6.1rem;
+  background-color: #abc8e8;
+  margin-left: 1rem;
+  font-size: 2rem;
+  font-weight: 1000;
+  cursor: pointer;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
 `;
