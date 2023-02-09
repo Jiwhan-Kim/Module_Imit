4a2023ba286ec92025e2d724f1ba26f91dea7482
KimJW <kimjw@KimJWs-MacBook-Air-2.local>
2023-02-09 23:36:52 +0900

    23.02.09. Ast23 Update Info

--- src/components/molecules/info/index.jsx
+++ src/components/molecules/info/index.jsx
@@ -11,8 +11,16 @@ function Info() {
         <TitleStr>공지사항</TitleStr>
         <div style={{ height: "0.5rem" }} />
         <NormalStr>
-          본 사이트는 연세대학교 시스템반도체공학과 Module에서 제작한
+          본 페이지는 연세대학교 제1대 시스템반도체공학과 학생회 Module에서 제작한
           '모의수강신청' 사이트입니다.
+        </NormalStr>
+        <div style={{ height: "0.5rem" }} />
+        <NormalStr>
+          제2대 학생회 Conduct, 학회 Asterisk 2023에서 운영 및 관리하고 있습니다.
+        </NormalStr>
+        <div style={{ height: "0.5rem" }} />
+        <NormalStr>
+          2023년 행정 시스템 변화로 인해 이 페이지와 실제 수강신청 페이지에 차이가 있을 수 있습니다.
         </NormalStr>
         <div style={{ height: "0.5rem" }} />
         <NormalStr
@@ -29,14 +37,23 @@ function Info() {
             setOpen(list0);
           }}
         >
-          <NormalStr>제작: 김다운, 김지환 (시반공21)</NormalStr>
+          <NormalStr>제작: 김다운, 김지환 (시스템반도체공학과 21)</NormalStr>
           <div style={{ height: "0.5rem" }} />
           <NormalStr>ReactJS제작</NormalStr>
+          <div style={{ height: "2.5rem" }} />
+          <NormalStr style={{ fontWeight: "700", fontSize: "1.7rem" }}>Update..</NormalStr>
+          <div style={{ height: "0.5rem" }} />
+          <NormalStr>22.02.15. Module Develop - 모의수강신청 시스템 제작</NormalStr>
+          <div style={{ height: "0.5rem" }} />
+          <NormalStr>22.08.18. Module Update - 모의수강신청 시스템 내용 수정</NormalStr>
+          <div style={{ height: "0.5rem" }} />
+          <NormalStr>23.02.09. Asterisk Update - 모의수강신청 시스템 내용 수정</NormalStr>
+
         </div>
       </div>
       <div>
         <NormalStr>
-          대기순번제 기간: 2022.08.24. 09:00 ~ 2022.08.24. 17:00
+          대기순번제 기간: 2023.02.23. 09:00 ~ 2023.02.23. 17:00
         </NormalStr>
       </div>
     </InfoBox>
